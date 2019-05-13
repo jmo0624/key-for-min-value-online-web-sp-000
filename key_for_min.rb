2d9cac301d0085825ac7b5f77e21c21826b88317
Josh Morris <jmo0624@gmail.com>
2019-05-13 23:31:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+silverware = {spoons: 10, forks: 20, knives: 15}
 
-def key_for_min_value(name_hash)
+def key_for_min_value(silverware)
+  
 
 end
