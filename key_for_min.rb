917b56eae0490f6adfbf5a4727123ba434c83346
Josh Morris <jmo0624@gmail.com>
2019-05-14 00:16:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,11 @@ def key_for_min_value(hash)
   lowest_value = nil
   hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
-      
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  
   
 
 end
