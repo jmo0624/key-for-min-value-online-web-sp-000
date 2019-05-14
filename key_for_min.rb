7c4b510368a9116e94b06c005e2fae539ab51d96
Josh Morris <jmo0624@gmail.com>
2019-05-14 00:14:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 silverware = {spoons: 10, forks: 20, knives: 15}
 
-def key_for_min_value(silverware)
+def key_for_min_value(hash)
+  
   
 
 end
